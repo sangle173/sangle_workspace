3c05d6d70acc5e023440fab04ed3680e12260cd0
Sang Le <leducsang.10dt2@gmail.com>
2025-05-07 22:22:45 +0700

    update

---
+++ local-cloud/src/main/java/com/example/local_cloud/config/FaviconConfig.java
@@ -0,0 +1,15 @@
+package com.example.local_cloud.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class FaviconConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/favicon.ico")
+                .addResourceLocations("classpath:/public/");
+    }
+} 
